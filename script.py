a55e54b6d91de18cfd8094dd2d3a6c105a16591a
sazankaa <satorobi9280@gmail.com>
2025-02-19 14:52:37 +0900

    iterdirに()を付けた

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from pypdf import PdfWriter
 from pathlib import Path
 
 #pdfsフォルダの中身を順に取り出し、名前でソートしリストとして返す。
-files = sorted(Path("./pdfs").iterdir)
+files = sorted(Path("./pdfs").iterdir())
 
 #書き込みオブジェクトの作成
 writer = PdfWriter()
