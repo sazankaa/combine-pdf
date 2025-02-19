e5370ed78f9f464a39989973c18a7e4f9bb121f6
sazankaa <satorobi9280@gmail.com>
2025-02-19 13:34:32 +0900

    初期コード

---
+++ script.py
@@ -0,0 +1,14 @@
+from pypdf import PdfWriter
+from pathlib import Path
+
+#pdfsフォルダの中身を順に取り出し、名前でソートしリストとして返す。
+files = sorted(Path("./pdfs").iterdir)
+
+#書き込みオブジェクトの作成
+writer = PdfWriter()
+
+for pdf in files:
+    #pdfをwriterに順に追加していく
+    writer.append(pdf)
+
+writer.write("MERGED.pdf")
